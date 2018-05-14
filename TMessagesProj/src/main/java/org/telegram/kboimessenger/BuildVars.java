be3862acb4c3e40ff1a01cafd54e0caa9b3f0fa5
sandeshbaliga <sandeshbaliga96@gmail.com>
2018-05-14 16:06:24 +0530

    First commit

---
+++ TMessagesProj/src/main/java/org/telegram/kboimessenger/BuildVars.java
@@ -0,0 +1,25 @@
+/*
+ * This is the source code of Telegram for Android v. 4.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2017.
+ */
+
+package org.telegram.kboimessenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static int BUILD_VERSION = 1155;
+    public static String BUILD_VERSION_STRING = "4.6";
+    public static int APP_ID = 175034; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "8ff2bb3587de4a078727ffaf2d64ed35"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "b6587f1453aa4d6d9f097e252413d080";
+    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String GOOGLE_API_KEY = "";
+    public static String FOURSQUARE_API_VERSION = "20150326";
+}
